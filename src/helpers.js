6f7f80eaf8bbe20086785ba8d143069db8a5b8f2
RutB <42755206+RutB@users.noreply.github.com>
2021-01-31 21:59:57 +0000

    byrja á styles

--- src/helpers.js
+++ src/helpers.js
@@ -1,2 +1,13 @@
 //age
 //duration
+
+app.locals.parseDate = (d) => {
+    const date = new Date(d);
+  
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+  
+    return `${day}.${month}.${year}`;
+  };
+  

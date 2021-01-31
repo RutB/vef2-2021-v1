ee1b35196d762c44c9cec277e0f02541ee71242f
RutB <42755206+RutB@users.noreply.github.com>
2021-01-31 20:12:42 +0000

    kanna

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const videos = require('./src/videos.js');
 
 const app = express();
-
+//er þetta ékki poottþétt
 // app.set('views', path.join(__dirname, 'views'));
  app.set('view engine', 'ejs');
 
